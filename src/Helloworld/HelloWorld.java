270e324120a08cfd4aec0ac13c55852f7289c1c7
MM <margret.magnusdottir@utb.ecutbildning.se>
2021-03-03 14:39:35 +0100

    hello

--- src/Helloworld/HelloWorld.java
+++ src/Helloworld/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
        System.out.println("Hello");
-        System.out.println("World");
+        System.out.println("Hello");
         System.out.println("!");
 
 // Det finns mer som t och n men jag kan inte skriva bakåtslash på grund av dåligt tagentbort
